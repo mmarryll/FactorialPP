44f528dc2f023fa051a9d607d3bdfa1a84aa315b
Mariya Lagun <108588991+mmarryll@users.noreply.github.com>
2022-11-09 10:57:53 +0300

    Update UserController.java

--- UserController.java
+++ UserController.java
@@ -1,18 +1,46 @@
-package com.example.resttest;
+package com.example.factorial;
 
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 
-    @RestController
-    public class UserController {
+@RestController
+//@RequestMapping("api/factorial")
+public class UserController {
 
-        @GetMapping("/getFacts")
-        public int getFact(@RequestParam(value = "number", defaultValue = "0") int number) {
-            User mynum(number);
-            for (int i = 2; i <= mynum ; i++) {
-                return calculateFact();
+    @GetMapping("/factorialOfN")
+
+        public long fact(@RequestParam(value = "number", defaultValue = "0") long num)
+        {
+            long result = 1;
+            if(num == 0)
+            {
+                return 1;
             }
+            for(int i = 1; i <= num; i++)
+            {
+                result *= i;
+            }
+            return result;
         }
-
-    }
+    @GetMapping("/factorialsTillN")
+        public ArrayList<Long> factorials(@RequestParam(value="number", defaultValue = "0") long num)
+        {
+            ArrayList<Long> result_ =new ArrayList<>();
+            long result=1;
+            if(num == 0)
+            {
+                result_.add(1L);
+                return result_;
+            }
+            for(int i = 1; i <= n; i++)
+            {
+                result*=i;
+                result_.add(result);
+            }
+            return result_;
+        }
+}
