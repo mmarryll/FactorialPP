17461e21174b5642763d8cb2179094205a1e525e
Mariya Lagun <108588991+mmarryll@users.noreply.github.com>
2023-01-15 23:40:14 +0300

    Update FactorialTests.java

--- FactorialTests.java
+++ FactorialTests.java
@@ -6,8 +6,57 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class FactorialTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void fact() {
+        var controller = new UserController();
+        long n=6;
+        var result=controller.fact(n);
+        var resultStatusCode=result.getStatusCode();
+        assertEquals(HttpStatus.OK, resultStatusCode);
+    }
+    @Test
+    void fact1() {
+        var controller = new UserController();
+        long n=0;
+        var result=controller.fact(n);
+        var resultStatusCode=result.getStatusCode();
+        assertEquals(HttpStatus.OK, resultStatusCode);
+    }
+    @Test
+    void fact2() {
+        var controller = new UserController();
+        long n=-1;
+        var result=controller.fact(n);
+        var resultStatusCode=result.getStatusCode();
+        assertEquals(HttpStatus.BAD_REQUEST, resultStatusCode);
+    }
 
+
+    @Test
+    void facts() {
+        var controller = new UserController();
+        long n=6;
+        var result=controller.facts(n);
+        var resultStatusCode=result.getStatusCode();
+        assertEquals(HttpStatus.OK, resultStatusCode);
+
+    }
+    @Test
+    void facts1() {
+        var controller = new UserController();
+        long n=1;
+        var result=controller.facts(n);
+        var resultStatusCode=result.getStatusCode();
+        assertEquals(HttpStatus.OK, resultStatusCode);
+
+    }
+    @Test
+    void facts2() {
+        var controller = new UserController();
+        long n=0;
+        var result=controller.facts(n);
+        var resultStatusCode=result.getStatusCode();
+        assertEquals(HttpStatus.BAD_REQUEST, resultStatusCode);
+
+    }
 }
